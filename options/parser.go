f4ff76c9ffbc61f0d25276f9f4058e02a7616a7c
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-12-20 21:19:20 +0100

    :sparkles: Adds version flag

--- options/parser.go
+++ options/parser.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+const (
+	Version = "1.0"
+)
+
 func Parse() Options {
 	parser := argparse.NewParser("print", "Runs DDOS attack on desired server")
 	parser.ExitOnHelp(true)
@@ -13,18 +17,28 @@ func Parse() Options {
 	delay := parser.Int("d", "delay", &argparse.Options{Required: false, Help: "Packet Delay", Default: 0})
 	maxRetryCount := parser.Int("r", "max-retry-count", &argparse.Options{Required: false, Help: "Max retry count, 0 for none", Default: 0})
 	workerCount := parser.Int("w", "worker-count", &argparse.Options{Required: false, Help: "Worker thread count, 0 for none", Default: 0})
-	address := parser.String("a", "address", &argparse.Options{Required: true, Help: "Address to DDOS"})
+	address := parser.String("a", "address", &argparse.Options{Required: false, Help: "Address to DDOS", Default: ""})
 	message := parser.String("m", "message", &argparse.Options{Required: false, Help: "Message to send", Default: ""})
 	logLevel := parser.Selector("l", "log-level", []string{"NONE", "ERROR", "WARN", "INFO"}, &argparse.Options{Required: false, Help: "Log level", Default: "INFO"})
 	http := parser.Flag("H", "http", &argparse.Options{Required: false, Help: "Whether to use HTTP", Default: false})
 	ignoreError := parser.Flag("i", "ignore-error", &argparse.Options{Required: false, Help: "Whether to ignore error", Default: false})
 	noColor := parser.Flag("N", "no-color", &argparse.Options{Required: false, Help: "Whether to display colored output", Default: false})
+	version := parser.Flag("v", "version", &argparse.Options{Required: false, Help: "Display version info", Default: false})
 
 	err := parser.Parse(os.Args)
 	if err != nil {
-		fmt.Printf("%v", parser.Usage(err))
+		fmt.Print(parser.Usage(err))
 	}
 
+	if *version {
+		fmt.Printf("DDOS version %v\n", Version)
+		os.Exit(0)
+	}
+
+	if *address == "" {
+		fmt.Print(parser.Usage("[-a|--address] is missing"))
+		os.Exit(1)
+	}
 	if *http && *message != "" {
 		fmt.Printf("%v", parser.Usage("Cannot use `[-H|--http]` while using `[-m|--message]`"))
 		os.Exit(1)
