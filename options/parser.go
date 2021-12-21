754d386cf01073f5c041ba7831eb8e08627dcbbe
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-12-21 11:09:21 +0100

    :zap: Fixes typo

--- options/parser.go
+++ options/parser.go
@@ -14,7 +14,7 @@ func Parse() Options {
 	parser := argparse.NewParser("ddos", "Runs DDOS attack on desired server")
 	parser.ExitOnHelp(false)
 
-	delay := parser.Int("d", "delay", &argparse.Options{Required: false, Help: "Packet delay", Default: 0})
+	delay := parser.Int("d", "delay", &argparse.Options{Required: false, Help: "Packet delay, 0 for none", Default: 10})
 	maxRetryCount := parser.Int("r", "max-retry-count", &argparse.Options{Required: false, Help: "Max retry count, 0 for none", Default: 0})
 	workerCount := parser.Int("w", "worker-count", &argparse.Options{Required: false, Help: "Worker thread count, 0 for none", Default: 0})
 	address := parser.String("a", "address", &argparse.Options{Required: false, Help: "Address to DDOS", Default: ""})
@@ -22,7 +22,7 @@ func Parse() Options {
 	logLevel := parser.Selector("l", "log-level", []string{"NONE", "ERROR", "WARN", "INFO"}, &argparse.Options{Required: false, Help: "Log level", Default: "INFO"})
 	http := parser.Flag("H", "http", &argparse.Options{Required: false, Help: "Use HTTP message", Default: false})
 	ignoreError := parser.Flag("i", "ignore-error", &argparse.Options{Required: false, Help: "Do not terminate program on error", Default: false})
-	noColor := parser.Flag("N", "no-color", &argparse.Options{Required: false, Help: "Display colored output", Default: false})
+	noColor := parser.Flag("N", "no-color", &argparse.Options{Required: false, Help: "Do not display colored output", Default: false})
 	version := parser.Flag("v", "version", &argparse.Options{Required: false, Help: "Display version info", Default: false})
 	outputFile := parser.File("o", "output", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600, &argparse.Options{Required: false, Help: "Additional output file", Default: os.Stdout})
 
